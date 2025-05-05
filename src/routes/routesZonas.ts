b1f9ab0bb6091fbb561bf650acb9cedfe1a30f8b
gonzaderito <gonzaderito04@gmail.com>
2025-05-05 19:27:15 -0300

    Documentados endpoints de zonas con Swagger

--- src/routes/routesZonas.ts
+++ src/routes/routesZonas.ts
@@ -1,12 +1,111 @@
-import express from 'express';
-import { getAllZonas, getZonaById, createZona, updateZona, deleteZona } from '../controllers/controllerZona';
+import express from "express";
+import {
+  getAllZonas,
+  getZonaById,
+  createZona,
+  updateZona,
+  deleteZona,
+} from "../controllers/controllerZona";
 
 const router = express.Router();
 
-router.get('/', getAllZonas);
-router.get('/:id', getZonaById);
-router.post('/', createZona);
-router.put('/:id', updateZona);
-router.delete('/:id', deleteZona);
+router.get("/", getAllZonas);
+/**
+ * @swagger
+ * /zona:
+ *   get:
+ *     summary: Obtener todas las zonas
+ *     tags: [Zonas]
+ *     responses:
+ *       200:
+ *         description: Lista de zonas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *
+ */
+router.get("/:id", getZonaById);
+/**
+ * @swagger
+ * /zona/{id}:
+ *   get:
+ *     summary: Obtener una zona por ID
+ *     tags: [Zonas]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID de la zona
+ *     responses:
+ *       200:
+ *         description: Zona encontrada
+ *
+ *       404:
+ *         description: Zona no encontrada
+ */
+router.post("/", createZona);
+/**
+ * @swagger
+ * /zona:
+ *   post:
+ *     summary: Crear una nueva zona
+ *     tags: [Zonas]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *
+ *     responses:
+ *       201:
+ *         description: Zona creada
+ */
+router.put("/:id", updateZona);
 
+/**
+ * @swagger
+ * /zona/{id}:
+ *   put:
+ *     summary: Actualizar una zona existente
+ *     tags: [Zonas]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la zona
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *
+ *     responses:
+ *       200:
+ *         description: Zona actualizada
+ *       404:
+ *         description: Zona no encontrada
+ */
+router.delete("/:id", deleteZona);
+/**
+ * @swagger
+ * /zona/{id}:
+ *   delete:
+ *     summary: Eliminar una zona
+ *     tags: [Zonas]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID de la zona
+ *     responses:
+ *       200:
+ *         description: Zona eliminada
+ *       404:
+ *         description: Zona no encontrada
+ */
 export default router;
