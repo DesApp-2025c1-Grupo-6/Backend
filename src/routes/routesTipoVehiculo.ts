48db7440fe813c504551278bddaba79f5f69f551
cesaremilianogerace <cesaremiliano@gmail.com>
2025-05-18 16:20:15 -0300

    CRUD tipoVehiculo

--- src/routes/routesTipoVehiculo.ts
+++ src/routes/routesTipoVehiculo.ts
@@ -1,8 +1,27 @@
 import express from 'express';
-import { getAllTiposVehiculo } from '../controllers/controllerTipoVehiculo';
+import { 
+  getAllTiposVehiculo,
+  getTipoVehiculoById,
+  createTipoVehiculo,
+  updateTipoVehiculo,
+  deleteTipoVehiculo
+} from '../controllers/controllerTipoVehiculo';
 
 const router = express.Router();
 
+// Todos
 router.get('/', getAllTiposVehiculo);
+
+// Uno por ID
+router.get('/:id', getTipoVehiculoById);
+
+// Crear
+router.post('/', createTipoVehiculo);
+
+// Actualizar
+router.put('/:id', updateTipoVehiculo);
+
+// Eliminar
+router.delete('/:id', deleteTipoVehiculo);
 
 export default router;
